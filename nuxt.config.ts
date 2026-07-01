a1d3ce423ee55477d6c060b4f513e45c70f6445f
segun mentis <mentis2010@gmail.com>
2026-07-01 00:56:40 -0600

    fix: switch to vuetify-nuxt-module, fix SSR error, set port 1080

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,30 +1,31 @@
-import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
-
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
+  compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
+  ssr: false,
 
-  build: {
-    transpile: ['vuetify'],
-  },
+  modules: ["vuetify-nuxt-module"],
 
-  modules: [
-    (_options, nuxt) => {
-      nuxt.hooks.hook('vite:extendConfig', (config) => {
-        // @ts-expect-error
-        config.plugins.push(vuetify({ autoImport: true }))
-      })
+  vuetify: {
+    moduleOptions: {
+      styles: true,
     },
-  ],
-
-  vite: {
-    vue: {
-      template: {
-        transformAssetUrls,
+    vuetifyOptions: {
+      theme: {
+        defaultTheme: "light",
       },
     },
   },
 
-  css: ['vuetify/styles', '@mdi/font/css/materialdesignicons.css'],
-})
+  css: ["@mdi/font/css/materialdesignicons.css"],
+
+  vite: {
+    optimizeDeps: {
+      include: ["@vue/devtools-core", "@vue/devtools-kit"],
+    },
+  },
+
+  devServer: {
+    port: 1080,
+  },
+});
